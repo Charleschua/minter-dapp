24326f665ea9b05a5d8342c3ee59e5d4d69089f7
Charleschua <charleschua19@gmail.com>
2022-08-17 17:03:34 +0800

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x48e91Ce8fa84514E9F2BFbC939ac1785DBaFB918"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
